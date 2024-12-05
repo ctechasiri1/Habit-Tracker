743652184f848d76a1f4a57b36c23613fd316cb0
Chiraphat <ctechasiri@gmail.com>
2024-12-04 20:11:10 -0800

    Initial Commit

---
+++ Habit Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by Chiraphat Techasiri on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Chiraphat Techasiri on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
